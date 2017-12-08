94852236f8d3205d0c27a1ee9a5f89a205153309
Brock McElroy <28877984+brxck@users.noreply.github.com>
2017-12-07 19:54:41 -0700

    Implement Piece#initialize

--- lib/pieces.rb
+++ lib/pieces.rb
@@ -1,5 +1,13 @@
 module Pieces
+  # Generic chess piece class
   class Piece
+    attr_accessor :position
+
+    def initialize(x, y)
+      @x = x
+      @y = y
+    end
+
     def on_board?(x, y)
       result = true
       [x, y].each { |coord| result = false unless (0..7).cover?(coord) }
