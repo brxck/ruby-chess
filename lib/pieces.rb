4d8cb354e094b37b6d176266e7e0c29077dff85c
Brock McElroy <28877984+brxck@users.noreply.github.com>
2017-12-07 19:54:41 -0700

    Implement Piece#move

--- lib/pieces.rb
+++ lib/pieces.rb
@@ -13,5 +13,14 @@ module Pieces
       [x, y].each { |coord| result = false unless (0..7).cover?(coord) }
       result
     end
+
+    def move(x, y)
+      if on_board?(x, y) && in_moveset?(x, y)
+        @x = x
+        @y = y
+      end
+      false
+    end
   end
+
 end
